4f93927a810137adb8a6ec50aaf713f99bbcde5c
coder-webb <codey.webb@outlook.com>
2023-08-16 11:35:43 -0700

    added some comments and simplified both methods

--- Main.java
+++ Main.java
@@ -5,21 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(ConvertFromBase10(2023, 16));
+        System.out.println(ConvertToBase10("7E7", 16));
     }
 
     public static String ConvertFromBase10(int number, int newBase)
     {
         String result = "";
         
-        while (number > 0)
+        while (number > 0) // continue running until number = 0 when performing number /= newBase
         {
-            int digit = number % newBase; // number modulo new base
-            if (digit < 10)
+            int digit = number % newBase; // get remainder of number modulo newBase
+            
+            if (digit < 10) // if remainder is a digit
                 result = digit + result; // add digit to the beginning of result string
-            else
-            {
-                result = (char)(digit - 10 + 'A') + result;
-            }
+            else // else if remainder is a letter
+                result = (char)(digit - 10 + 'A') + result; // add letter
             
             number /= newBase; // divide number by new base
         }
@@ -31,16 +31,13 @@ public class Main {
     {
         int result = 0;
 
-        for (char c : number.toCharArray()) {
+        for (char c : number.toCharArray()) // traverse the input number
+        {
             result *= oldBase;
-            if (c < 10)
-            {
-                result += c;
-            }
+            if (c >= '0' && c <= '9')
+                result += c - '0';
             else
-            {
                 result += (c - 'A' + 10);
-            }
         }
 
         return result;
